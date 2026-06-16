ac6a05181e543c534f63fb5e484a2005308b101f
bblhieu-spec <bblhieu@gmail.com>
2026-06-16 17:21:09 +0700

    Update hung script.lua

--- hung script.lua
+++ hung script.lua
@@ -1,8 +1,8 @@
 -- Bản quyền thuộc về Ngô Ngọc Hùng --
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Hệ Thống",
+    Title = "Thông báo",
     Text = "Bản quyền thuộc về Ngô Ngọc Hùng",
-    Duration = 6
+    Duration = 5
 })
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubBeta.lua"))()
